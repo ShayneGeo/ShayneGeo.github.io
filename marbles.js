87dc9b15b225d75e4c878da2f91e03e7167ff8fb
ShayneGeo <92595748+ShayneGeo@users.noreply.github.com>
2026-08-05 13:22:24 -0600

    Add files via upload

---
+++ marbles.js
@@ -0,0 +1,10 @@
+window.LOCAL_MARBLES = [
+  {
+    src: "MARBLES/M1/M1.webp",
+    alt: "Blue, teal, and gold galaxy-style glass marble"
+  },
+  {
+    src: "MARBLES/M2/M2.png",
+    alt: "Handmade glass marble"
+  }
+];
